da394131e6578763da4fa067dbfac537f4b17865
Gab1109 <47677890+GabrielMorin1109@users.noreply.github.com>
2021-06-16 17:38:53 -0400

    Update run_extraction_asynchrone.sh

--- run_extraction_asynchrone.sh
+++ run_extraction_asynchrone.sh
@@ -3,7 +3,8 @@
 # find script in computer
 ERA5_scrip_API_path=$(find . -name ERA5_scrip_API.sh)
 merge_variables_path=$(find . -name merge_variables.sh)
-
+ERA5_asynchrone_path=$(find . -name merge_variables.sh)
 #run script
 python3 $ERA5_scrip_API_path
 bash $merge_variables_path
+Rscript $ERA5_asynchrone_path
